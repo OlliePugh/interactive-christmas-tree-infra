9cc2f46297e4b37904bff6b816b9d7403fa41868
Ollie Pugh <oliver.pugh@mac.com>
2022-11-11 22:35:35 +0000

    Init commit

---
+++ functions/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "google"],
+  rules: {
+    quotes: ["error", "double"],
+    "quote-props": ["error", "as-needed"],
+    "object-curly-spacing": ["error", "always"],
+  },
+};
