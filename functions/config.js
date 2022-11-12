430bc65cb5113707ad20b0caf490e1ca847ad944
Ollie Pugh <oliver.pugh@mac.com>
2022-11-12 23:12:51 +0000

    Introduced cooldown to modify a square

---
+++ functions/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  modifierCooldown: 60000,
+};
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 let functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
+const config = require("./config.js");
 functions = functions.region("europe-west1");
 
-exports.changeSquare = functions.https.onCall((data, context) => {
+exports.changeSquare = functions.https.onCall(async (data, context) => {
   if (!context.auth)
     return { status: "error", code: 401, message: "Not signed in" };
 
@@ -18,14 +19,49 @@ exports.changeSquare = functions.https.onCall((data, context) => {
     time: new Date(),
   };
 
-  const userActionsRef = admin.firestore().collection("user-actions").doc(uid); // get the user actions from firestore
+  console.log("Starting transaction");
+  const userDocRef = admin.firestore().collection("user-actions").doc(uid);
+  try {
+    await admin.firestore().runTransaction(async (transaction) => {
+      const userActionsDoc = await transaction.get(userDocRef);
+
+      if (!userActionsDoc.exists) {
+        throw "Document does not exist!";
+      }
+      console.log("newAction", JSON.stringify(action));
+      const sortedActions = userActionsDoc
+        .data()
+        .actions.filter((act) => act.boardId === boardId);
+
+      const mostRecentAction = sortedActions[sortedActions.length - 1];
+      console.log("mostRecentAction", JSON.stringify(mostRecentAction));
+
+      if (mostRecentAction) {
+        // if its not null
+        const timeDelta =
+          new Date().getTime() -
+          new Date(mostRecentAction.time.seconds * 1000).getTime();
+
+        console.log("Time Delta", timeDelta);
+
+        if (timeDelta < config.modifierCooldown) {
+          return Promise.reject("cooldown");
+        }
+      }
+
+      // been over a minute let the user place it again
+      console.log("Adding new action to the list of actions");
+      await transaction.update(userDocRef, {
+        actions: admin.firestore.FieldValue.arrayUnion(action),
+      });
+    });
+  } catch (e) {
+    console.error(e);
+    return { status: "too-many", code: 429 };
+  }
+
   // add the action to the array of actions
-  userActionsRef.update({
-    actions: admin.firestore.FieldValue.arrayUnion(action),
-  });
-
   admin.database().ref(`board${boardId}/${id}`).set({ color, id }); // update the pixel
-
   return { status: "ok", code: 200 };
 });
 
