fea1af6545d380b764913b9fd69e83f6a3cc3c0f
Ollie Pugh <oliver.pugh@mac.com>
2022-12-26 23:25:23 +0000

    Closed the project

--- functions/index.js
+++ functions/index.js
@@ -2,34 +2,33 @@ let functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 functions = functions.region("europe-west1");
-const storeBaubleBmp = require("./store-bauble-bmp");
 
-const getBaubleBmp = require("./get-bauble-bmp");
+// const getBaubleBmp = require("./get-bauble-bmp");
 
-exports.changeSquare = require("./change-square/index.js");
+// exports.changeSquare = require("./change-square/index.js");
 
-exports.resetBoard = require("./reset-board/index.js");
+// exports.resetBoard = require("./reset-board/index.js");
 
-exports.resetLights = require("./reset-lights/index.js");
+// exports.resetLights = require("./reset-lights/index.js");
 
-exports.changeLight = require("./change-light/index.js");
+// exports.changeLight = require("./change-light/index.js");
 
-exports.getFullLights = require("./get-full-lights/index.js");
+// exports.getFullLights = require("./get-full-lights/index.js");
 
 exports.createProfile = require("./create-profile");
 
-const _createCacheLights = async () => {
-  const snapshot = await admin.database().ref(`lights/data`).once("value");
-  const data = snapshot.val();
+// const _createCacheLights = async () => {
+//   const snapshot = await admin.database().ref(`lights/data`).once("value");
+//   const data = snapshot.val();
 
-  const bucket = admin.storage().bucket();
-  try {
-    await bucket.file("lights.json").save(JSON.stringify(data));
-  } catch (e) {
-    console.log(e);
-    console.log("File upload failed");
-  }
-};
+//   const bucket = admin.storage().bucket();
+//   try {
+//     await bucket.file("lights.json").save(JSON.stringify(data));
+//   } catch (e) {
+//     console.log(e);
+//     console.log("File upload failed");
+//   }
+// };
 
 // exports.createCacheLights = functions.pubsub.schedule("* * * * *").onRun(() => {
 //   _createCacheLights();
@@ -44,70 +43,70 @@ const _createCacheLights = async () => {
 //   getBaubleBmp();
 // });
 
-const _baubleBmpCronJob = async (boardId) => {
-  const bmpData = await getBaubleBmp({ admin, boardId });
-  storeBaubleBmp(boardId, bmpData);
-};
+// const _baubleBmpCronJob = async (boardId) => {
+//   const bmpData = await getBaubleBmp({ admin, boardId });
+//   storeBaubleBmp(boardId, bmpData);
+// };
 
-exports.baubleBmpCronJob1 = functions.pubsub
-  .schedule("0 * * * *")
-  .onRun(async () => {
-    await Promise.all([
-      _baubleBmpCronJob(1),
-      _baubleBmpCronJob(2),
-      _baubleBmpCronJob(3),
-    ]);
-  });
+// exports.baubleBmpCronJob1 = functions.pubsub
+//   .schedule("0 * * * *")
+//   .onRun(async () => {
+//     await Promise.all([
+//       _baubleBmpCronJob(1),
+//       _baubleBmpCronJob(2),
+//       _baubleBmpCronJob(3),
+//     ]);
+//   });
 
-exports.baubleBmpCronJob2 = functions.pubsub
-  .schedule("10 * * * *")
-  .onRun(async () => {
-    await Promise.all([
-      _baubleBmpCronJob(1),
-      _baubleBmpCronJob(2),
-      _baubleBmpCronJob(3),
-    ]);
-  });
+// exports.baubleBmpCronJob2 = functions.pubsub
+//   .schedule("10 * * * *")
+//   .onRun(async () => {
+//     await Promise.all([
+//       _baubleBmpCronJob(1),
+//       _baubleBmpCronJob(2),
+//       _baubleBmpCronJob(3),
+//     ]);
+//   });
 
-exports.baubleBmpCronJob3 = functions.pubsub
-  .schedule("20 * * * *")
-  .onRun(async () => {
-    await Promise.all([
-      _baubleBmpCronJob(1),
-      _baubleBmpCronJob(2),
-      _baubleBmpCronJob(3),
-    ]);
-  });
+// exports.baubleBmpCronJob3 = functions.pubsub
+//   .schedule("20 * * * *")
+//   .onRun(async () => {
+//     await Promise.all([
+//       _baubleBmpCronJob(1),
+//       _baubleBmpCronJob(2),
+//       _baubleBmpCronJob(3),
+//     ]);
+//   });
 
-exports.baubleBmpCronJob4 = functions.pubsub
-  .schedule("30 * * * *")
-  .onRun(async () => {
-    await Promise.all([
-      _baubleBmpCronJob(1),
-      _baubleBmpCronJob(2),
-      _baubleBmpCronJob(3),
-    ]);
-  });
+// exports.baubleBmpCronJob4 = functions.pubsub
+//   .schedule("30 * * * *")
+//   .onRun(async () => {
+//     await Promise.all([
+//       _baubleBmpCronJob(1),
+//       _baubleBmpCronJob(2),
+//       _baubleBmpCronJob(3),
+//     ]);
+//   });
 
-exports.baubleBmpCronJob5 = functions.pubsub
-  .schedule("40 * * * *")
-  .onRun(async () => {
-    await Promise.all([
-      _baubleBmpCronJob(1),
-      _baubleBmpCronJob(2),
-      _baubleBmpCronJob(3),
-    ]);
-  });
+// exports.baubleBmpCronJob5 = functions.pubsub
+//   .schedule("40 * * * *")
+//   .onRun(async () => {
+//     await Promise.all([
+//       _baubleBmpCronJob(1),
+//       _baubleBmpCronJob(2),
+//       _baubleBmpCronJob(3),
+//     ]);
+//   });
 
-exports.baubleBmpCronJob6 = functions.pubsub
-  .schedule("50 * * * *")
-  .onRun(async () => {
-    await Promise.all([
-      _baubleBmpCronJob(1),
-      _baubleBmpCronJob(2),
-      _baubleBmpCronJob(3),
-    ]);
-  });
+// exports.baubleBmpCronJob6 = functions.pubsub
+//   .schedule("50 * * * *")
+//   .onRun(async () => {
+//     await Promise.all([
+//       _baubleBmpCronJob(1),
+//       _baubleBmpCronJob(2),
+//       _baubleBmpCronJob(3),
+//     ]);
+//   });
 
 // exports.testbaubleBmpCronJob = functions.https.onRequest(async (req, res) => {
 //   await _baubleBmpCronJob(req.query.id);
